f1ff302f5eda208349fb47158ff86c96f9f2446b
Diego Kuwahara de Castro <diego.castro@compasso.com.br>
2019-09-13 16:51:22 -0300

    Criação do Player, Término da modelagem do Stage e Movimentações para Andar e Pular

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float posicaoMinimaX;
+    public float posicaoMinimaY;
+
+    private Vector2 velocity;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = player.position.x < posicaoMinimaX ? base.transform.position.x : player.position.x;
+        float posY = player.position.y < posicaoMinimaY ? base.transform.position.y : player.position.y;
+                     
+        base.transform.position = new Vector3(posX, posY, base.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walkSpeed;
+    public Transform groundCheck;
+    public float jumpStrength;
+
+    private bool isGrounded = false;
+    private bool isWalking = false;
+    private bool isFacingRight = true;
+    private bool isJumping = false;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private new Rigidbody2D rigidbody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        isGrounded = Physics2D.Linecast(base.transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        this.Animations();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(direction * walkSpeed, rigidbody2D.velocity.y);
+
+        if ((direction < 0f && isFacingRight) || (direction > 0f && !isFacingRight))
+            this.Flip();
+
+        if (isJumping)
+        {
+            rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, jumpStrength));
+            isJumping = false;
+        }
+    }
+
+    private void Animations()
+    {
+        animator.SetBool("isWalking", isGrounded && rigidbody2D.velocity.x != 0f);
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        base.transform.localScale = new Vector3(-base.transform.localScale.x, base.transform.localScale.y, base.transform.localScale.z);
+    }
+}
