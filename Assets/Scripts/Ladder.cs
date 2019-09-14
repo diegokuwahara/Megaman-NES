8d0d95c06fcff429a2400bcdc86e6751a6538f4c
Diego Kuwahara de Castro <diego.castro@compasso.com.br>
2019-09-14 16:49:11 -0300

    Adicionado detecção e animação de subir/descer escadas

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private void ScriptThatTurnsPlatformBackOn()
+    {
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GroundCheck") && Input.GetAxisRaw("Vertical") < 0)
+        {
+            Debug.Log("Entrou");
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            Invoke("ScriptThatTurnsPlatformBackOn", 0.5f);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,17 @@ public class Player : MonoBehaviour
     public float walkSpeed;
     public Transform groundCheck;
     public float jumpStrength;
+    public LayerMask ladderLayer;
 
     private bool isGrounded = false;
     private bool isWalking = false;
     private bool isFacingRight = true;
     private bool isJumping = false;
+    private bool isClimbing = false;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
     private new Rigidbody2D rigidbody2D;
+    private float defaultGravityScale;
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,7 @@ public class Player : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        defaultGravityScale = rigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
@@ -40,7 +44,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float direction = Input.GetAxis("Horizontal");
+        float direction = Input.GetAxisRaw("Horizontal");
         rigidbody2D.velocity = new Vector2(direction * walkSpeed, rigidbody2D.velocity.y);
 
         if ((direction < 0f && isFacingRight) || (direction > 0f && !isFacingRight))
@@ -51,12 +55,48 @@ public class Player : MonoBehaviour
             rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, jumpStrength));
             isJumping = false;
         }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(groundCheck.position, Vector2.up, 10, ladderLayer);
+        Debug.DrawRay(groundCheck.position, Vector2.up, Color.red);
+
+        if (hitInfo.collider != null)
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                isClimbing = true;
+                transform.position = new Vector2(hitInfo.transform.position.x, transform.position.y);
+            }else if (Input.GetButtonDown("Jump") || (isGrounded && Input.GetAxisRaw("Horizontal") != 0))
+            {
+                isClimbing = false;
+            }
+
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        if (isClimbing && hitInfo.collider != null)
+        {
+            float vDirection = Input.GetAxisRaw("Vertical");
+            animator.speed = vDirection == 0 ? 0 : 1;
+
+            rigidbody2D.velocity = new Vector2(0f, vDirection * 3);
+            rigidbody2D.gravityScale = 0;
+        }
+        else
+        {
+            animator.speed = 1;
+            rigidbody2D.gravityScale = defaultGravityScale;
+        }
     }
 
     private void Animations()
     {
         animator.SetBool("isWalking", isGrounded && rigidbody2D.velocity.x != 0f);
         animator.SetBool("isGrounded", isGrounded);
+        animator.SetBool("isClimbing", isClimbing);
+        animator.SetInteger("yVel", (int)rigidbody2D.velocity.y);
     }
 
     private void Flip()
